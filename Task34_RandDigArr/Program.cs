4e7cb5dd0a5990f73c5ee6cbb3dc987f6be1ab03
Dmitriy <kazany.dmitry@gmail.com>
2022-09-08 07:05:47 +0300

    add task34 random number array, whow even

---
+++ Task34_RandDigArr/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void RandomArray(int[] a)
+{
+Random FillNum = new Random();
+for(int i = 0; i < a.Length; i++) a[i] = FillNum.Next(100, 1000);
+Console.WriteLine(String.Join(", ", a));
+}
+
+int EvenNumber(int[] a)
+{
+int count = 0;
+for(int i=0; i < a.Length; i++)
+{
+    if(a[i] % 2 == 0) count++;
+}
+return count;
+}
+
+int[] array = new int[10];
+RandomArray(array);
+Console.WriteLine(EvenNumber(array));
