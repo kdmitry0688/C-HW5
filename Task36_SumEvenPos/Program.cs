0947f001781d600f7f3ea09d9df11e383695c072
Dmitriy <kazany.dmitry@gmail.com>
2022-09-08 07:13:39 +0300

    add task36 summ even numbers of array

---
+++ Task36_SumEvenPos/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void RandomArray(int[] a)
+{
+Random FillNum = new Random();
+for(int i = 0; i < a.Length; i++) a[i] = FillNum.Next(1, 100);
+Console.WriteLine(String.Join(", ", a));
+}
+
+int Summ(int[] a)
+{
+int summ = 0;
+for(int i = 1; i < a.Length; i += 2)
+    {
+	summ += a[i];
+	}
+	return summ;
+}
+
+int[] array = new int[10];
+RandomArray(array);
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях массива: " + Summ(array));
